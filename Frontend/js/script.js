0c33c4c6ff720c30760f89236a5c2cff0afacf27
Monteiro <142056009+monteirosai@users.noreply.github.com>
2024-04-22 18:52:25 -0300

    Update script.js

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://project1-1-wgcl.onrender.com")
     websocket.onmessage = processMessage
 }
 
